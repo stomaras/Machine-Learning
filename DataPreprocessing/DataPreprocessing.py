7afcd7b5462bc37d6d833157e002f41eb7726b66
stomaras <sptomaras@yahoo.gr>
2020-01-15 16:06:29 +0200

    Create DataPreprocessing.py

---
+++ DataPreprocessing/DataPreprocessing.py
@@ -0,0 +1,13 @@
+# Data Preprocessing
+
+#Importing the libraries
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#Importing the dataset
+
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 3].values
