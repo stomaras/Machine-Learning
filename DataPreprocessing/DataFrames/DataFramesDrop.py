b41ef0a6a490babfb70220a9c1370e407d1653d5
stomaras <sptomaras@yahoo.gr>
2020-01-22 12:13:14 +0200

    Update DataFramesDrop.py

--- DataPreprocessing/DataFrames/DataFramesDrop.py
+++ DataPreprocessing/DataFrames/DataFramesDrop.py
@@ -36,3 +36,21 @@ print(df1.head())
 # 4. Dropping the Multiple Column of a dataset based on the Column Index
 df2 = df1.drop(df1.columns[[1,3]],axis=1)
 print(df2.head())
+
+
+# 5. Deleting rows of a dataset and copying the resultant dataset to a new dataframe
+df3 = df2.drop(df2.index[[1,3]])
+print(df3.head())
+
+
+# 6. Drop the Duplicate Values based on the conditions
+df3 = df3[df3['Embarked'] != 'S'].head()
+print(df3.head())
+
+
+Dup = df3.drop_duplicates().head()
+
+Dup1 = df3.drop_duplicates(subset=['Embarked'], keep='last')
+
+print(Dup1)
+
